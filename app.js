1230fe5d4dbf1ba3fca039741237d3ac68e3fa2e
Jarno <7944858+myerd@users.noreply.github.com>
2021-07-14 10:33:28 +0300

    Modified dburl for heroku

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var app = express();
 //Set up mongoose connection
 var mongoose = require('mongoose');
 
-var mongoDB = 'mongodb+srv://<user>:<pass>@cluster0.tytfc.mongodb.net/local_db?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://<user>:<pass>@cluster0.tytfc.mongodb.net/local_db?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
